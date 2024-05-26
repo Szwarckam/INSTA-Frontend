bc3931693b2a1c03c2b41b1f43b379addd1b358f
Szwarckam <kamil.szwarc.06@gmail.com>
2024-05-26 14:52:43 +0200

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+//static load
+//@ oznacza katalog /src
+
+import Home from "@/views/HomeView.vue";
+import About from "@/views/AboutView.vue";
+
+//lazy load - renderuje się tylko gdy jest potrzebny a nie z góry
+
+const NotFoundView = () => import("@/views/NotFoundView.vue");
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFoundView",
+      component: NotFoundView,
+    },
+  ],
+});
+
+export default router;
